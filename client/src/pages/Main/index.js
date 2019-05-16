2941d861113693c018d6e1a4f02128d9e8125318
Ronny Reinhold <ronny@bnu.com.br>
2019-05-16 20:29:43 -0300

    [change] launche page at frontend

--- client/src/pages/Main/index.js
+++ client/src/pages/Main/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
 import './style.css';
+import logo from '../../assets/early-acess.gif';
 
 export default class Main extends Component {
     state = {
@@ -22,7 +23,20 @@ export default class Main extends Component {
 
     render(){
         return(
-            <div id="container">
+            <div id="main-container">
+                <div className="row white">
+                    <header>
+                        <img src={logo} alt="" />
+                        <h2>SpaceX</h2>
+                    </header>
+                </div>
+                <div className="row black">
+                    <h2>Launches</h2>
+                    <div>
+                        <h3>s</h3>
+                    </div>
+                </div>
+                
                 { this.state.launches && this.state.launches.map(launch => (
                     <p className="text" key={launch.id}>{launch.mission}</p>
                 ))}
