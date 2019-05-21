4e0ebfd70df671b48cc51c2936830ebf58a875ec
Ronny Reinhold <ronny@bnu.com.br>
2019-05-21 13:06:37 -0300

    [Add] rocket description

--- client/src/pages/Launch/index.js
+++ client/src/pages/Launch/index.js
@@ -9,26 +9,41 @@ export default class Launch extends Component {
     }
 
     async componentDidMount(){
-        
         //const launch = this.props.match.params.id;
         const { id } = this.props.match.params;
-        const res = await api.post('/graphql', {
-            query: `{ getLaunch(id: ${id}){
-                id,
-                mission,
-                success,
-                rocket{
+        if(id !== ""){
+            await api.post('/graphql', {
+                query: `{ getLaunch(id: ${id}){
                     id,
-                    name,
-                    type
-                }
-            }}`
-        });
-        this.setState({ launch: res.data.data.getLaunch, isLoading: false});
+                    mission,
+                    success,
+                    rocket{
+                        id,
+                        name,
+                        type
+                    }
+                }}`
+            })
+            .then(res => this.setState({ launch: res.data.data.getLaunch, isLoading: false}))
+            .catch(err => (console.log(err.message)));
+        }
+       
+
+        if(this.state.launch.rocket){
+            await api.post('/graphql', {
+                query: `{ getRocket(id: "${this.state.launch.rocket.id}"){
+                    description
+                }}`
+            })
+            .then(res => this.setState({launch: { ...this.state.launch, 
+                rocket: { ...this.state.launch.rocket, 
+                    description: res.data.data.getRocket.description }}}))
+            .catch(err => console.log(err.message));
+        }
     }
 
     render(){
-        const { mission, rocket} = this.state.launch;
+        const { id, mission, rocket} = this.state.launch;
         return(
             <div className="">
                 {this.state.isLoading 
@@ -37,6 +52,7 @@ export default class Launch extends Component {
                 <Fragment>
                     <h3>{mission}</h3>
                     <p>{rocket.name}</p>
+                    <p>{rocket.description}</p>
                 </Fragment> 
                 }
                 
