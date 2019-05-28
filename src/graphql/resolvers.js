44b1e69edebb4482349e3c130b215d83a7c07563
Ronny Reinhold <ronny@bnu.com.br>
2019-05-28 08:00:23 -0300

    [Remove] resolvers return

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,9 +1,9 @@
 export default {
     Query: {
         hello: () => 'World',
-        getLaunch: (_, { id }, { dataSources }) => { return dataSources.Launch.getLaunch(id) },
-        getLaunches: (_, args, { dataSources }) => { return dataSources.Launch.getLaunches() },
-        getRocket: (_, { id }, { dataSources }) => { return dataSources.Rocket.getRocket(id) },
-        getRockets: (_, args, { dataSources }) => { return dataSources.Rocket.getRockets() }
+        getLaunch: (_, { id }, { dataSources }) => dataSources.Launch.getLaunch(id),
+        getLaunches: (_, args, { dataSources }) => dataSources.Launch.getLaunches(),
+        getRocket: (_, { id }, { dataSources }) => dataSources.Rocket.getRocket(id),
+        getRockets: (_, args, { dataSources }) =>  dataSources.Rocket.getRockets()
     }
 }
